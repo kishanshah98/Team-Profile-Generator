aa50c770dba33392c5fd1ed13a610bf91476a76f
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-06-14 19:47:54 -0400

    Created tests for Employee class and made some minor changes in class file

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -2,52 +2,70 @@
   REMOVE ALL THE COMMENTS BEFORE SUBMITTING YOUR HOMEWORK
 ***********************************************************/
 
-const { exportAllDeclaration } = require("@babel/types");
+const { newExpression } = require("@babel/types");
+const exp = require("constants");
+const { getSystemErrorName } = require("util");
 const Employee = require("../lib/Employee");
 
 // test instantiation of employee instance
-test("", () => {
+test("Should instantiate employee instance", () => {
+  const result = new Employee();
 
+  expect(typeof(result)).toBe("object");
 });
 
 // test constructor initialized with a name
-test("should create an object with a name if provided valid argument", () => {
-  const personName = new Employee(1, "Jack", "email@gmail.com");
-
-  expect(personName.name).toEqual("Jack");
+test("Should set name properly via constructor", () => {
+  const result = "Nick";
+  const employee = new Employee(result);
+  
+  expect(employee.name).toBe(result);
 });
 
 // test constructor initialized wtih an id
-test("should create an object with an ID if provided valid argument ", () => {
-  const personName = new Employee(1, "Jack", "email@gmail.com");
+test("Should set id properly via constructor", () => {
+  const value = 100;
+  const employee = new Employee("Name", value);
 
-  expect(personName.id).toEqual(1);
+  expect(employee.id).toBe(value);
 });
 
 // test constructor initialized with an email address
-test("should create an object with an email address if provided valid argument", () => {
-  const personName = new Employee(1, "Jack", "email@gmail.com");
+test("Should set email properly via constructor", () => {
+  const result = "email@gmail.com"
+  const employee = new Employee("Name", 5, result);
 
-  expect(personName.email).toEqual("email@gmail.com");
-
+  expect(employee.email).toBe(result);
 });
 
 // test get name method
-test("", () => {
+test("Should get name with getName() function", () => {
+  const result = "Nick";
+  const employee = new Employee(result);
 
+  expect(employee.getName()).toBe(result);
 });
 
 // test get Id method
-test(")", () => {
+test("Should get id with getId() function", () => {
+  const result = 100;
+  const employee = new Employee("Name", 100);
 
+  expect(employee.getId()).toBe(result);
 });
 
 // test get email method
-test("", () => {
+test("Should get email with getEmail() function", () => {
+  const result = "email@gmail.com";
+  const employee = new Employee("Name", 100, result);
 
+  expect(employee.getEmail()).toBe(result);
 });
 
 // test get role method. it should return "Employee"
-test("", () => {
+test("Should get role with getRole() function", () => {
+  const result = "Employee";
+  const employee = new Employee("Name", 100, "email@gmail.com");
 
+  expect(employee.getRole()).toBe(result);
 });
