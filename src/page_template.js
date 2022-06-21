de466e62214b33b0f67abde600542f4dd32c063c
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-06-21 18:44:40 -0400

    Made github username become a link and email to work too

--- src/page_template.js
+++ src/page_template.js
@@ -8,7 +8,7 @@ const buildCards = function(employees) {
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">ID: ${manager.getId()}</li>
-                            <li class="list-group-item">Email: ${manager.getEmail()} </li>
+                            <li class="list-group-item">Email: <a href=mailto:${manager.getEmail()}>${manager.getEmail()}</a></li>
                             <li class="list-group-item">Office Number: ${manager.getOfficeNumber()} </li>
                         </ul>
                     </div>
@@ -24,8 +24,8 @@ const buildCards = function(employees) {
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">ID: ${engineer.getId()}</li>
-                            <li class="list-group-item">Email: ${engineer.getEmail()}</li>
-                            <li class="list-group-item">Github: ${engineer.getGithub()}</li>
+                            <li class="list-group-item">Email: <a href=mailto:${engineer.getEmail()}>${engineer.getEmail()}</a></li>
+                            <li class="list-group-item">Github: <a href="https://github.com/${engineer.getGithub()}" target="_blank"> ${engineer.getGithub()}</a></li>
                         </ul>
                     </div>
     `
@@ -40,7 +40,7 @@ const buildCards = function(employees) {
                         </div>
                         <ul class="list-group list-group-flush">
                             <li class="list-group-item">ID: ${intern.getId()}</li>
-                            <li class="list-group-item">Email: ${intern.getEmail()}</li>
+                            <li class="list-group-item">Email: <a href=mailto:${intern.getEmail()}>${intern.getEmail()}</a></li>
                             <li class="list-group-item">School: ${intern.getSchool()}</li>
                         </ul>
                     </div>
