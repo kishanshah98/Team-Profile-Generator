32c5750078b0b8ea0678e4d67168eec9f3099b14
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-06-21 17:03:23 -0400

    Completed page template and created most functions in main index file

--- index.js
+++ index.js
@@ -18,13 +18,14 @@ const path = require("path");
 const fs = require('fs');
 
 // Imports page template function and assign the function to a variable
-const template = require('./src/page_template');
+const generateHTML = require('./src/page_template');
 
 // Create variables for the output folder and the html file name
-
+const DIST_DIR = path.resolve(__dirname, 'dist');
+const distPath = path.join(DIST_DIR, 'team.html');
 
 // Create an empty array to store the team member objects
-const employeeList = [];
+const teamList = [];
 
 // Create an empty array to store employee IDs to be used to check for the dupliates
 const employeeIds = [];
@@ -34,42 +35,148 @@ const employeeIds = [];
 // **********************
 
 // Call the starter function
+init();
 
 // starter function to start the program
-//
-//  1a. call create manager function
-//
-//  1b. function to create manager
-//      - inquire user to enter manager's name, id, email, and office number
-//      - in .then callback, create manager object from the Manager class by initializing it with the properties in answer object
-//      - push the manager object to the empty team memeber object array
-//      - push the manager's id to the empty id array
-//      - call create team function
-//
-//  2. function to create team (called from create manager, add engineer, add intern functions)
-//      - inquire user to pick employee from the list of engineer, intern, or exit (done with adding employees)
-//      - in .then callback, based on user's choice, call add engineer or add intern or, for the choice of exit, call build team function
-//
-//  3. function to add engineer
-//      - inquire user to enter engineer's name, id, email, and github
-//      - in .then callbackl create engineer object from the Engineer class by initializing it with the properties in answer object
-//      - push the engineer object to the empty team member object array
-//      - push the engineer's id to the empty id array
-//      - call create team function
-//
-//  4. function to add intern
-//      - inquire user to enter intern's name, id, email, and school
-//      - in .then callback, create intern object from the intern class by initializing it with the properties in answer object
-//      - push the intern object to the empty team member object array
-//      - push the intern's id to the empty id array
-//      - call create team function
-//
-//  5. function build team
-//      - check if the output folder path already exists.
-//      -   if not, create it
-//      - call page template function passing the team member object array as input argument
-//      - write the returned template function to the output
-//
-//      - Hint: fs.existsSync, fs.mkdirSync, and fs.writeFileSync
+function init() {
+  console.log("Starts program")
 
+  //  1a. call create manager function
+  createManager();
 
+  //  1b. function to create manager
+  //      - inquire user to enter manager's name, id, email, and office number
+  //      - in .then callback, create manager object from the Manager class by initializing it with the properties in answer object
+  //      - push the manager object to the empty team memeber object array
+  //      - push the manager's id to the empty id array
+  //      - call create team function
+  function createManager() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: "What is the manager's name?",
+          name: 'name',
+        },
+        {
+          type: 'input',
+          message: "What is the manager's id?",
+          name: 'id',
+        },
+        {
+          type: 'input',
+          message: "What is the manager's email?",
+          name: 'email',
+        },
+        {
+          type: 'input',
+          message: "What is the manager's office number?",
+          name: 'officeNumber',
+        },
+      ])
+      .then(function (response) {
+        console.log(response);
+        const addManager = new Manager(response.name, response.id, response.email, response.officeNumber);
+        teamList.push(addManager);
+        employeeIds.push(addManager.id);
+        createTeam();
+        console.log(employeeIds);
+        console.log(teamList);
+      })
+  };
+
+  //  2. function to create team (called from create manager, add engineer, add intern functions)
+  //      - inquire user to pick employee from the list of engineer, intern, or exit (done with adding employees)
+  //      - in .then callback, based on user's choice, call add engineer or add intern or, for the choice of exit, call build team function
+  //
+  function createTeam() {
+    console.log("Create Team function working");
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          message: "Would you like to add any of the following to your team?",
+          choices: ['Engineer', 'Intern', 'None, I would like to exit'],
+          name: 'role',
+        },
+      ])
+      .then(function (response) {
+        console.log(response);
+        if (response.role === 'Engineer') {
+          addEngineer();
+          console.log("Engineer function called");
+        } else if (response.role === 'Intern') {
+          addIntern();
+          console.log("Intern function called");
+        } else  {
+          buildTeam();
+          console.log("Build team function called");
+        };
+      });
+  };
+
+  function addEngineer() {
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          message: "What is the engineer's name?",
+          name: 'name',
+        },
+        {
+          type: 'input',
+          message: "What is the engineer's id?",
+          name: 'id',
+        },
+        {
+          type: 'input',
+          message: "What is the engineer's email?",
+          name: 'email',
+        },
+        {
+          type: 'input',
+          message: "What is the engineer's github?",
+          name: 'github',
+        },
+      ])
+      .then(function (response) {
+        console.log(response);
+        const addEngineer = new Engineer(response.name, response.id, response.email, response.github);
+        teamList.push(addEngineer);
+        employeeIds.push(addEngineer.id);
+        createTeam();
+        console.log(employeeIds);
+        console.log(teamList);
+      })
+  };
+  //  3. function to add engineer
+  //      - inquire user to enter engineer's name, id, email, and github
+  //      - in .then callbackl create engineer object from the Engineer class by initializing it with the properties in answer object
+  //      - push the engineer object to the empty team member object array
+  //      - push the engineer's id to the empty id array
+  //      - call create team function
+  //
+  function addIntern() {
+  }
+  //  4. function to add intern
+  //      - inquire user to enter intern's name, id, email, and school
+  //      - in .then callback, create intern object from the intern class by initializing it with the properties in answer object
+  //      - push the intern object to the empty team member object array
+  //      - push the intern's id to the empty id array
+  //      - call create team function
+  //
+  function buildTeam() {
+    if (!fs.existsSync(DIST_DIR)) {
+      fs.mkdirSync(DIST_DIR);
+    }
+    fs.writeFileSync(distPath, generateHTML(teamList), 'utf-8')
+  }
+  //  5. function build team
+  //      - check if the output folder path already exists.
+  //      -   if not, create it
+  //      - call page template function passing the team member object array as input argument
+  //      - write the returned template function to the output
+  //
+  //      - Hint: fs.existsSync, fs.mkdirSync, and fs.writeFileSync
+
+};
--- src/page_template.js
+++ src/page_template.js
@@ -1,7 +1,116 @@
 /**********************************************************
   REMOVE ALL THE COMMENTS BEFORE SUBMITTING YOUR HOMEWORK
 ***********************************************************/
+const buildCards = function(employees) {
+  function buildManager(manager) {
+    return `
+    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${manager.getName()}</h5>
+                            <p class="card-text">Manager</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${manager.getId()}</li>
+                            <li class="list-group-item">Email: ${manager.getEmail()} </li>
+                            <li class="list-group-item">Office Number: ${manager.getOfficeNumber()} </li>
+                        </ul>
+                    </div>
+    `
+  };
 
+  function buildEngineer(engineer) {
+    return `
+    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title">${engineer.getName()}</h5>
+                            <p class="card-text">Engineer</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${engineer.getId()}</li>
+                            <li class="list-group-item">Email: ${engineer.getEmail()}</li>
+                            <li class="list-group-item">Github: ${engineer.getGithub()}</li>
+                        </ul>
+                    </div>
+    `
+  };
+
+  function buildIntern(intern) {
+    return `
+    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h5 class="card-title"> ${intern.getName()}</h5>
+                            <p class="card-text">Intern</p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">ID: ${intern.getId()}</li>
+                            <li class="list-group-item">Email: ${intern.getEmail()}</li>
+                            <li class="list-group-item">School: ${intern.getSchool()}</li>
+                        </ul>
+                    </div>
+    `
+  };
+
+  const allCards = [];
+  const managerHTML = employees.filter(employee => employee.getRole() === 'Manager')
+  .map(manager => buildManager(manager));
+  allCards.push(managerHTML);
+
+  const engineerHTML = employees.filter(employee => employee.getRole() === 'Engineer')
+  .map(engineer => buildEngineer(engineer))
+  .join('');
+  allCards.push(engineerHTML);
+
+  const internHTML = employees.filter(employee => employee.getRole() === 'Intern')
+  .map(intern => buildIntern(intern))
+  .join('');
+  allCards.push(internHTML);
+
+  return allCards.join('');
+
+};
+
+module.exports = employees => {
+  return `
+  <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style.css">
+    <script href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"></script>
+    <title>Team Profile</title>
+</head>
+
+<body>
+    <header>
+        <div class="row">
+            <div class="col-12 col-md-12 jumbotron jumbotron-fluid">
+                <div class="jumbotron-container">
+                    <h1 class="text-center">My Team</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="row">
+            <div class="col-12 col-md-12">
+                <div class="card-container">
+                ${buildCards(employees)}
+                </div>
+            </div>
+        </div>
+    </main>
+
+</body>
+
+</html>
+  `
+}
 // Step 1. export function to generate entire html page with template literal
 // Step 2. function to create team template
 // Step 3. functions to generate manager, engineer, and intern templates
